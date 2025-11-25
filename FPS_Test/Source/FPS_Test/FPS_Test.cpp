2cfd922db172e4571f5b7cda8900138a4ffbe927
Thijmen <thijmen.mennegat@gmail.com>
2025-11-25 15:36:13 +0100

    project and gitignore added

--- FPS_Test/Source/FPS_Test/FPS_Test.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "FPS_Test.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPS_Test, "FPS_Test" );
